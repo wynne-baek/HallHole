e8571594e279b49d25209e4d239a5649673051d6
Jekwan <kjaguan@gmail.com>
2022-07-25 00:24:37 +0900

    [Add] styled-components 테스트 컴포넌트

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 
+import ButtonTest from "./ButtonTest";
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello 홀홀!</h1>
         <h2>리액트로 만드는 홀홀</h2>
+        <ButtonTest></ButtonTest>
       </div>
     );
   }
