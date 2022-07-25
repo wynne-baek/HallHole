1036f386254fa33f34f75d74153ae1cf0ddbffd5
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-07-25 13:31:33 +0900

    [Add] project 생성

---
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/HallholeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.hallhole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallholeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HallholeApplication.class, args);
+    }
+
+}
---
+++ BE/hallhole/src/test/java/com/ssafy/hallhole/HallholeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.hallhole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallholeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
