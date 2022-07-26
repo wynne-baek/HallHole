f027fda41998bb7e1f2e3dad0a02b2590beb17bd
Jekwan <kjaguan@gmail.com>
2022-07-26 13:34:29 +0900

    [Add] ProfileImageLarge 컴포넌트

---
+++ FE/src/components/atom/ProfileImageLarge.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class ProfileImageLarge extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ProfileImageLarge;
