17c0c22778efb6f4082e2a2a7245ef715c4a5c90
Binzify <beingbright95@gmail.com>
2022-07-26 17:37:05 +0900

    [Add] 하단선 구분/탭선택선추가

---
+++ FE/src/components/atom/CategorySelectedPrimary.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function CategoryLineDark() {
+  return (
+    <Box
+      sx={{
+        width: 90,
+        height: 5,
+        backgroundColor: '#e37373',
+      }}
+    />
+  );
+}
