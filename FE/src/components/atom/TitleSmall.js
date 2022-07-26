3f4974f5c5ff70d2389b2e500d688619eb3cff0d
cheecrma <cheecrma@gmail.com>
2022-07-26 22:07:18 +0900

    [Update] Title text사이즈 컴포넌트

---
+++ FE/src/components/atom/TitleSmall.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const  CustomText = styled.text`
+font-family: IBM Plex Sans, sans-serif;
+font-weight: bold;
+font-size: 16px;
+color: black;
+`;
+
+export default function UnStyledTextSimple({children}) {
+  return (
+    <CustomText>{children}</CustomText>
+  );
+}
