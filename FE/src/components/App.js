93e43642ba829ddbf3e1c995df5ece714016059b
김제관 <jekwan@kakao.com>
2022-07-27 17:36:14 +0900

    [Add] mui 테마

--- FE/src/components/App.js
+++ FE/src/components/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#e85d65",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello 홀홀!</h1>
-      <h2>리액트로 만드는 홀홀</h2>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <h1>Hello 홀홀!</h1>
+        <h2>리액트로 만드는 홀홀</h2>
+      </div>
+    </ThemeProvider>
   );
 }
