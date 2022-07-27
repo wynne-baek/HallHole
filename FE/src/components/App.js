a27d1b1181d131ba404f5dc4a5daa6ac6b808b37
Jekwan <kjaguan@gmail.com>
2022-07-27 14:10:30 +0900

    [Update] package.json
    sass 관련 패키지와 styled-component 제거

--- FE/src/components/App.js
+++ FE/src/components/App.js
@@ -1,19 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
-import ButtonTest from "./ButtonTest";
-import MUITest from "./MUITest";
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello 홀홀!</h1>
-        <h2>리액트로 만드는 홀홀</h2>
-        <ButtonTest></ButtonTest>
-        <MUITest></MUITest>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>Hello 홀홀!</h1>
+      <h2>리액트로 만드는 홀홀</h2>
+    </div>
+  );
 }
-
-export default App;
