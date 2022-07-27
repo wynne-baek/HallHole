a22ac344c90aaecb35e4b8d978bbc0978ec64004
Binzify <beingbright95@gmail.com>
2022-07-27 17:59:51 +0900

    [Update]어쩔

---
+++ FE/src/components/atom/CategoryLineNegative.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function CategoryLineNegative() {
+  return (
+    <Box
+      sx={{
+        width: 360,
+        height: 3,
+        backgroundColor: '#E1E1E1',
+      }}
+    />
+  );
+}
