324836a5641e2b9068509041298d83e7ae1dab0e
SANGBIN IM <97656304+Binzify@users.noreply.github.com>
2022-07-28 14:15:37 +0900

    [Add]포스터사이즈sx설정추가

---
+++ FE/src/components/atom/PosterSize.js
@@ -0,0 +1,32 @@
+export const posterLarge = {
+  width: 300,
+  height: 400,
+  //maxWidth: 300,
+  //maxHeight: 400,
+};
+
+export const posterLargeOutlined = {
+  width: 300,
+  height: 400,
+  //maxWidth: 300,
+  //maxHeight: 400,
+  border: 1,
+}
+
+export const posterMedium = {
+  width: 180,
+  height: 240,
+  //maxWidth: 180,
+  //maxHeight: 240,
+}
+
+export const posterSmall = {
+  width: 100,
+  height: 134,
+  //maxWidth: 100,
+  //maxHeight: 134,
+}
+
+export const posterBlur = {
+  backdropFilter: 'blur(3px)'
+}
