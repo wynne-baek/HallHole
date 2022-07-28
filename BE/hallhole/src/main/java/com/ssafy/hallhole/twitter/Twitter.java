8f70c8fb5baf6cabaf15c020a39f95b8d290078f
QuiD <wodnd101@naver.com>
2022-07-28 10:04:23 +0900

    [Add] twitter Docs 추가

--- BE/hallhole/src/main/java/com/ssafy/hallhole/twitter/Twitter.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/twitter/Twitter.java
@@ -1,6 +1,7 @@
 package com.ssafy.hallhole.twitter;
 
 import com.sun.istack.NotNull;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.Column;
@@ -19,14 +20,18 @@ public class Twitter {
 
     @Id
     @Column(length = 25)
+    @ApiModelProperty(example = "트윗 ID")
     private String id;
 
     @NotNull
+    @ApiModelProperty(example = "트윗 내용")
     private String contents;
 
+    @ApiModelProperty(example = "예매 링크")
     private String url;
 
     @NotNull
+    @ApiModelProperty(example = "트윗 작성 시간")
     private LocalDateTime time;
 
 }
--- BE/hallhole/src/main/java/com/ssafy/hallhole/twitter/controller/TwitterController.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/twitter/controller/TwitterController.java
@@ -2,7 +2,9 @@ package com.ssafy.hallhole.twitter.controller;
 
 import com.ssafy.hallhole.twitter.Twitter;
 import com.ssafy.hallhole.twitter.service.TwitterService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ public class TwitterController {
     private final TwitterService twitterService;
 
     @GetMapping
+    @ApiOperation(value="트윗 가져오기")
     public List<Twitter> getTweet(){
         return twitterService.findAllTweet();
     }
