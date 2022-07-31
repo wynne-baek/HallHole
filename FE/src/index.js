a8f26e9eab96387a870246291454e8d93b1e9e8f
jekwan <jekwan@kakao.com>
2022-08-01 00:08:49 +0900

    [Update] 전체 앱에 redux store 적용

--- FE/src/index.js
+++ FE/src/index.js
@@ -2,4 +2,14 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
 
-ReactDom.render(<App />, document.getElementById("app"));
+import rootReducer from "./states";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app"),
+);
