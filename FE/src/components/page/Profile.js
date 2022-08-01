5997a611461c4fb2184301ca235045041fb81416
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-01 17:33:17 +0900

    [Add] profile page 컴포넌트 추가

---
+++ FE/src/components/page/Profile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ProfileDetail from "../molecule/ProfileDetail";
+
+export default function Profile () {
+  return(
+    <ProfileDetail username="경원" usertag="#34534" follower="7" following="7" profile="일단 테스트용입니다" />
+  )
+}
