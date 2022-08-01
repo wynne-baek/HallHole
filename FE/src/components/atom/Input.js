0c801e83b3669e1b33f71fdfe8fdb0dfa4e12c8a
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-01 17:31:39 +0900

    [Update] 사용법 주석 추가

--- FE/src/components/atom/Input.js
+++ FE/src/components/atom/Input.js
@@ -3,6 +3,11 @@ import React from "react";
 import { TextField } from "@mui/material";
 
 export default function Input(props) {
+  /**
+   * props
+   *  - size : "large" 또는 "medium" 또는 "small" 
+   *  - onClick : 클릭 시 이벤트
+   */
   if (props.size === "large") {
     return <TextField sx={{ m:0.75, width: '75%' }} variant="outlined" color="primary" OnKeyPress={props.OnKeyPress} />
   } else if (props.size === "medium") {
