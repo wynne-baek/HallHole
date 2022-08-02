32a7bbc44548d38ede2d97d13235518901dd0dc4
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-02 13:11:31 +0900

    [Update] text 컴포넌트 수정

--- FE/src/components/atom/Text.js
+++ FE/src/components/atom/Text.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { styled } from "@mui/system";
 
-const CustomText = styled(`text`)({
+const CustomText = styled('span')({
+  // Text를 사용할 때 size와variant를 지정해 줘야하며
+  // 사이즈는 large, medium, small 있으며 각각 30px, 22px, 16px 크기이다.
+  // 색상은 primary, white, black 이다.
   color: "black",
+  display: "inline-block",
   fontFamily: "Helvetica Neue",
+  margin: 3,
 });
 
 export default function TextStyle({ children, variant }) {
