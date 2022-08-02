65f086ff7e4dc0a1e1d4ea82d7087689eace3219
cheecrma <cheecrma@gmail.com>
2022-08-02 11:56:05 +0900

    [Update] atom_Button

--- FE/src/components/atom/Button.js
+++ FE/src/components/atom/Button.js
@@ -1,126 +1,73 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import { styled } from "@mui/system";
 
-//  버튼은 크게 large, medium, small 사이즈로 구분되며
-// 색상은 primary, negative를 선택하게 된다.
-// 버튼에 sx를 적용하기 위해선 크기와 색상을 전부 지정해줘야하며
-// small 사이즈는 variant를 따로 지정하지 않았다.
+const style = {
+  m: 0.7,
+  p: 2,
+  borderRadius: 2,
+  boxShadow: 1,
+  fontWeight: "bold",
+  color: "white",
+};
+
+const ButtonCheck = styled(Button)(
+  ({ size, variant }) => `
+  width: ${getSizeWidth(size)};
+  height: ${getSizeHeight(size)};
+  background-color: ${getBackgroundColor(variant)};
+  &: hover{
+    background-color: ${getHoverColor(variant)};
+    color: "white";
+  }
+  fontWeight: "bold",
+  color: "white",
+  text-align: center;
+  `,
+);
+
+function getBackgroundColor(variant) {
+  switch (variant) {
+    case "primary":
+      return "#e37373";
+  }
+}
+
+function getHoverColor(variant) {
+  switch (variant) {
+    case "primary":
+      return "#e38f8f";
+  }
+}
+
+function getSizeWidth(size) {
+  switch (size) {
+    case "large":
+      return "380px";
+    case "medium":
+      return "150px";
+    case "small":
+      return "96px";
+  }
+}
+
+function getSizeHeight(size) {
+  switch (size) {
+    case "large":
+      return "60px";
+    case "medium":
+      return "45px";
+    case "small":
+      return "50px";
+  }
+}
 
 export default function ButtonStyle({ children, size, variant }) {
-  if (size === "large" && variant === "primary") {
-    return (
-      <Button
-        sx={{
-          color: "black",
-          bgcolor: "primary.main",
-          borderRadius: 2,
-          width: 380,
-          height: 60,
-          m: 0.7,
-          p: 2,
-          boxShadow: 1,
-          fontWeight: "bold",
-
-          "&:hover": {
-            color: "white",
-            backgroundColor: "secondary.main",
-          },
-        }}
-      >
-        {children}
-      </Button>
-    );
-    // 반복
-  } else if (size === "medium" && variant === "primary") {
-    return (
-      <Button
-        sx={{
-          color: "black",
-          bgcolor: "primary.main",
-          borderRadius: 2,
-          width: 150,
-          height: 45,
-          m: 0.7,
-          p: 2,
-          boxShadow: 1,
-          fontWeight: "bold",
-          "&:hover": {
-            color: "white",
-            backgroundColor: "secondary.main",
-          },
-        }}
-      >
-        {children}
-      </Button>
-    );
-    // 반복
-  } else if (size === "small") {
-    return (
-      <Button
-        sx={{
-          color: "black",
-          bgcolor: "primary.main",
-          borderRadius: 2,
-          width: 96,
-          height: 50,
-          m: 0.7,
-          p: 2,
-          boxShadow: 1,
-          fontWeight: "bold",
-          "&:hover": {
-            color: "white",
-            backgroundColor: "secondary.main",
-          },
-        }}
-      >
-        {children}
-      </Button>
-    );
-  } else if (size === "large" && variant === "negative") {
-    return (
-      <Button
-        sx={{
-          color: "black",
-          bgcolor: "negative.main",
-          borderRadius: 2,
-          width: 380,
-          height: 60,
-          m: 0.7,
-          p: 2,
-          boxShadow: 1,
-          fontWeight: "bold",
-
-          "&:hover": {
-            color: "white",
-            backgroundColor: "negative.main",
-          },
-        }}
-      >
-        {children}
-      </Button>
-    );
-  } else if (size === "medium" && variant === "negative") {
-    return (
-      <Button
-        sx={{
-          color: "black",
-          bgcolor: "negative.main",
-          borderRadius: 2,
-          width: 150,
-          height: 45,
-          m: 0.7,
-          p: 2,
-          boxShadow: 1,
-          fontWeight: "bold",
-
-          "&:hover": {
-            color: "white",
-            backgroundColor: "negative.main",
-          },
-        }}
-      >
-        {children}
-      </Button>
-    );
-  }
+  return (
+    //  버튼은 크게 large, medium, small 사이즈로 구분되며
+    // 색상은 현재 primary 를 선택하게 된다.(추후 색상 추가할 수 있다.)
+    <ButtonCheck sx={style} size={size} variant={variant}>
+      {children}
+    </ButtonCheck>
+  );
 }
