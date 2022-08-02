66156286293b49040b22a27148ab1aa472142136
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-02 15:55:55 +0900

    [Update] Navbar 컴포넌트 src 삭제, z-index 추가

--- FE/src/components/molecule/NavBar.js
+++ FE/src/components/molecule/NavBar.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import styled from '@emotion/styled';
+import React from "react";
+import Box from "@mui/material/Box";
+import styled from "@emotion/styled";
 
-import Logo from '../atom/Logo';
-import ProfileImage from '../atom/ProfileImage';
+import Logo from "../atom/Logo";
+import ProfileImage from "../atom/ProfileImage";
 
 // width 수정 필요. 상위 요소 정해
 const NavBox = styled(Box)(
   () => `
-  width: 100%;
+  width: 100;
   background: black;
   display:flex;
   justify-content: space-between;
@@ -17,21 +17,20 @@ const NavBox = styled(Box)(
   top: 0;
   left: 0;
   right: 0;
-  `
-)
-
+  `,
+);
 
 export default function NavBar() {
   return (
-    <NavBox>
+    <NavBox sx={{ zIndex: 1000 }}>
       {/* 홈 화면으로 돌아가는 링크 추가 */}
       {/* <Link to=""> */}
-        <Logo size="small" src="https://picsum.photos/100/150"/>
+      <Logo size="small" />
       {/* </Link> */}
       {/* 메뉴서랍? 오픈하기! */}
       {/* <Link to=""> */}
-        <ProfileImage type="thumb" src="" />
+      <ProfileImage type="thumb" src="" />
       {/* </Link> */}
     </NavBox>
-  )
-};
+  );
+}
