400e13b197dc55972886a88bdcc9027237fbe8ec
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-02 13:36:24 +0900

    [Update] LikePerformance 컴포넌트 완성

--- FE/src/components/molecule/LikePerformance.js
+++ FE/src/components/molecule/LikePerformance.js
@@ -1,14 +1,35 @@
-import { ImageList, ImageListItem, } from "@mui/material";
+import { ImageList, ImageListItem } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 
+import TextStyle from "../atom/Text";
+import CategoryDivider from "../atom/CategoryDivider";
+
+const flexContainer = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "nowrap",
+  padding: 0,
+  whiteSpace: "nowrap",
+  overflow: "auto",
+};
 
 export default function LikePerformances(props) {
-  // 좋아요 누른 공연 제목
-  <ImageList sx={{ flexWrap: 'nowrap' }}>
-    {props.likePerformances.map((item) => (
-      <ImageListItem key={item.img}>
-        <img src={`${item.img}`} alt={item.title}/>
-      </ImageListItem>
-    ))}
-  </ImageList>
+  return (
+    <Box sx={{ width: 1, marginY: 3, marginLeft: 1 }}>
+      <TextStyle size="medium" variant="primary">
+        좋아요 누른 공연
+      </TextStyle>
+      <CategoryDivider type="primary" />
+      <Box>
+        <ImageList style={flexContainer}>
+          {props.likePerformances.map(item => (
+            <ImageListItem key={item.id}>
+              <img key={item.id} src={item.img} alt={item.title} style={{ width: 150 }} />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box>
+    </Box>
+  );
 }
