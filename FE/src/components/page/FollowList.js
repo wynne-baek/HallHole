b2569eec57ac9e7b8431ef5d44ba84cd82ee5256
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-03 18:01:50 +0900

    [Add] FollowList 컴포넌트 추가

---
+++ FE/src/components/page/FollowList.js
@@ -0,0 +1,17 @@
+
+import { useRadioGroup } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+import TextStyle from "../atom/Text";
+import ProfileItem from "../molecule/ProfileItem";
+
+export default function FollowList() {
+  return (
+    <Box>
+      <TextStyle size="large" variant="black">{user.username}</TextStyle>
+      {/* CATEGORYSELECTOR */}
+      {/* profileitem map해서 보여주기 */}
+    </Box>
+  )
+};
