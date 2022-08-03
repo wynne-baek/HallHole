c9fadcd467ecb95ce3131935bfd363d80457a4d7
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-03 17:19:57 +0900

    [Update] MyActivity 수정

--- FE/src/components/molecule/MyActivity.js
+++ FE/src/components/molecule/MyActivity.js
@@ -1,37 +1,62 @@
 import React from "react";
+import { useState } from "react";
 import { List } from "@mui/material";
 import { Box } from "@mui/system";
 
-import CategorySelected from '../atom/CategorySelected'
+import CategorySelectButton from "./CategorySelectButton";
 import TextStyle from "../atom/Text";
-import ReviewItem from "./ReviewItem";
+import ProfileReviewItem from "./ProfileReviewItem";
 
-function getList(selected) {
-  if (selected === "review") {
-    return reviewList
-  } else if (selected === "comment") {
-    return commentList
-  } else if (selected === "reaction") {
-    return reactionList
-  }
+// function getList(selected) {
+//   if (selected === "review") {
+//     return reviewList
+//   } else if (selected === "comment") {
+//     return commentList
+//   } else if (selected === "reaction") {
+//     return reactionList
+//   }
+// }
+
+const ActivityCategory = [
+  { name: '후기', selected: true, },
+  { name: '댓글', selected: false, },
+  { name: '리액션', selected: false, },
+]
+
+// const Categories = ({ ActivityCategory, onClick }) =>
+// {ActivityCategory && ActivityCategory.map(
+//     (category) =>
+//       <CategorySelectButton key={category.name} category={category.name} selected={category.selected} onClick={onClick}></CategorySelectButton>
+//   )}
+
+const selectCategory = (e) => {
+  e.preventDefault();
+  // console.log(e.target.innerText)
+  // const { key } = e.currentTarget.dataset;
+  // console.log(e.currentTarget)
+  // console.log(e.currentTarget.dataset)
+  // MyActivityCategory[index].selected = !MyActivityCategory[index].selected;
 }
 
 
 export default function MyActivity(props) {
   return (
-    <Box sx={{ width: 1, marginY: 2, marginLeft: 1 }}>
-      <CategorySelected type="primary"></CategorySelected>
-      <CategorySelected type="primary"></CategorySelected>
-      <CategorySelected type="primary"></CategorySelected>
+    <Box sx={{ width: "95%", marginY: 2, marginLeft: 2 }}>
+      <Box sx={{ display: "flex" }}>
+        {ActivityCategory.map(
+          (category, i) =>
+            <CategorySelectButton id={i} key={i} category={category.name} selected={category.selected} onClick={selectCategory}></CategorySelectButton>
+        )}
+      </Box>
       <List>
-        {`${getList}`.map(item => (
-          <ReviewItem
+        {props.reviews.map(item => (
+          <ProfileReviewItem
             key={item.id}
             title={item.title}
             date={item.date}
             star_eval={item.star_eval}
             performance_name={item.performance_name}
-          ></ReviewItem>
+          ></ProfileReviewItem>
         ))}
       </List>
     </Box>
