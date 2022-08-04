34975bb16615011b2fd7e9f8b30b07101c8a6491
cheecrma <cheecrma@gmail.com>
2022-08-04 13:29:35 +0900

    [Update] main 추가

--- FE/src/configs/router.js
+++ FE/src/configs/router.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Intro from "../components/page/Intro-pinterest";
 import NoMatch from "../components/page/NoMatch";
 import Profile from "../components/page/Profile";
+import Main from "../components/page/Main";
 
 export default function RouterConfiguration() {
   return (
@@ -11,6 +12,7 @@ export default function RouterConfiguration() {
       <Route path="*" element={<NoMatch />} />
       <Route path="/" element={<Intro />} />
       <Route path="/signin" element={<Profile />} />
+      <Route path="/main" element={<Main />} />
     </Routes>
   );
 }
