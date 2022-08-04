3b1b6bdfb2e1e4413deef0383d9d6ba6fd209c54
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-04 11:39:03 +0900

    [Update] FollowList 카테고리, 목록 state 추가 및 기능 구현

--- FE/src/components/page/FollowList.js
+++ FE/src/components/page/FollowList.js
@@ -1,16 +1,12 @@
-import { List, useRadioGroup } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 
 import TextStyle from "../atom/Text";
 import ProfileItem from "../molecule/ProfileItem";
 import CategorySelectButton from "../molecule/CategorySelectButton";
 
 // 팔로워 팔로잉
-const follow = [
-  { name: "팔로워", followerCount: 5, selected: true }, //length(user.follower)},
-  { name: "팔로잉", followingCount: 6, selected: false }, //length(user.following)},
-];
+const target = ["팔로워", "팔로잉"];
 
 const followList = [
   { username: "경원" },
@@ -20,38 +16,48 @@ const followList = [
   { username: "경원" },
 ];
 const followerList = [
-  { username: "경원" },
-  { username: "경원" },
-  { username: "경원" },
-  { username: "경원" },
-  { username: "경원" },
+  { username: "효정" },
+  { username: "승리" },
+  { username: "재웅" },
+  { username: "제관" },
+  { username: "상빈" },
 ];
 
-// onClick에 달아둔 함수로, 누를 경우 아래의 리스트가 변경되어야 합니다!
-function selectFollowList(e) {
-  e.preventDefault();
-}
-
 export default function FollowList(props) {
+  const [selectedTarget, setSelectedTarget] = useState("팔로워");
+  const [selectedTargetList, setSelectedTargetList] = useState(followerList);
+
+  function selectFollowList(e) {
+    e.preventDefault();
+    // 선택한 버튼 active 처리
+    setSelectedTarget(e.target.innerText);
+    // 선택한 버튼에 따라 팔로워, 팔로잉 목록 변경(추후, user의 follow 목록 넣으면 됨!)
+    if (e.target.innerText === "팔로워") {
+      setSelectedTargetList(followerList);
+    } else {
+      setSelectedTargetList(followList);
+    }
+  }
+
   return (
     <Box sx={{ width: "95%", marginLeft: 1 }}>
       <TextStyle size="large" variant="black">
         {props.username}
       </TextStyle>
       {/* CATEGORY SELECTOR */}
-      <Box sx={{ display: "flex" ,marginY : 2 }}>
-        {follow.map((followlist, i) => (
+      <Box sx={{ display: "flex", marginY: 2 }}>
+        {target.map((item, i) => (
           <CategorySelectButton
             key={i}
-            category={followlist.name}
-            selected={followlist.selected}
+            category={item}
+            selected={selectedTarget === item}
             onClick={selectFollowList}
           ></CategorySelectButton>
         ))}
       </Box>
       {/* profileitem map해서 보여주기 */}
       <Box sx={{ marginTop: 1 }}>
-        {followList.map((profile, i) => (
+        {selectedTargetList.map((profile, i) => (
           <ProfileItem key={i} username={profile.username}></ProfileItem>
         ))}
       </Box>
