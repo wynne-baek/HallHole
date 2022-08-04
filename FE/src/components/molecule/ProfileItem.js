336d65955e61683ad6a2099ec8eedbb29ee59e9a
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-04 11:38:28 +0900

    [Update] ProfileItem onClick 함수 props로 변경

--- FE/src/components/molecule/ProfileItem.js
+++ FE/src/components/molecule/ProfileItem.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import ProfileImage from '../atom/ProfileImage';
-import TextStyle from '../atom/Text';
-import { CardHeader } from '@mui/material';
+import ProfileImage from "../atom/ProfileImage";
+import TextStyle from "../atom/Text";
+import { CardHeader } from "@mui/material";
 
 // props로 이미지 url과 username을 넘기면 됩니다
 // 클릭 이벤트는 추후에 달면 됩니다!
 
 export default function ProfileItem(props) {
   return (
-    <CardHeader sx={{ padding: 0.5 }}
-      avatar={<ProfileImage type="small" src="" onClick="" />} 
-      title={<TextStyle size="medium" variant="black">{props.username}</TextStyle>}>
-    </CardHeader>
-  )
+    <CardHeader
+      sx={{ padding: 0.5 }}
+      avatar={<ProfileImage type="small" src="" />}
+      title={
+        <TextStyle size="medium" variant="black">
+          {props.username}
+        </TextStyle>
+      }
+      onClick={props.onClick}
+    ></CardHeader>
+  );
 }
