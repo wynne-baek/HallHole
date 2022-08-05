5ce52c34ea6f89cc1603333845ca9bf818eb35ee
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-05 13:26:29 +0900

    [Update] ProfileItem 컴포넌트 팔로우/언팔로우 버튼 추가

--- FE/src/components/molecule/ProfileItem.js
+++ FE/src/components/molecule/ProfileItem.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import ProfileImage from "../atom/ProfileImage";
 import TextStyle from "../atom/Text";
-import { CardHeader } from "@mui/material";
+import { Box, CardHeader } from "@mui/material";
+import ButtonStyle from "../atom/Button";
 
 // props로 이미지 url과 username을 넘기면 됩니다
 // 클릭 이벤트는 추후에 달면 됩니다!
 
 export default function ProfileItem(props) {
+  const [follow, setFollow] = useState(true);
+  
+  // api 요청해 기본값 받는 로직 추가 
+  // useEffect(() => {
+  //   setFollow()
+  // })
+
+  function onChangeFollowButton(e) {
+    e.preventDefault();
+    setFollow(!follow)
+    console.log(follow)
+    // post 요청
+  }
+
   return (
+    <Box sx={{ width: 0.95, display: "flex", justifyContent:"space-between", alignItems:"center" }}>
     <CardHeader
       sx={{ padding: 0.5 }}
       avatar={<ProfileImage type="small" src="" />}
@@ -18,6 +34,9 @@ export default function ProfileItem(props) {
         </TextStyle>
       }
       onClick={props.onClick}
-    ></CardHeader>
+    >
+    </CardHeader>
+    <ButtonStyle size="small" variant={follow ? 'grey' : 'primary'} onClick={onChangeFollowButton}>{follow ? '언팔로우' : '팔로우'}</ButtonStyle>
+    </Box>
   );
 }
