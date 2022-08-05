a23ecf4e4eb35175aebee5ef2c42c63b9fa3de02
QuiD <wodnd101@naver.com>
2022-08-05 10:58:59 +0900

    [Update] CORS설정

--- BE/hallhole/src/main/java/com/ssafy/hallhole/config/WebConfig.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000");
+                .addMapping("/**")
+                .allowedOrigins("*");
     }
 
     @Override
