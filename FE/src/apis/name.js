425e32a706c007af7ad56199e74a616ee7d61118
jekwan <jekwan@kakao.com>
2022-08-05 15:41:06 +0900

    [Add] 유저 관련 API

--- FE/src/apis/name.js
+++
@@ -1,9 +0,0 @@
-import { ApiInstance } from "./api";
-
-const api = ApiInstance();
-
-async function fetchNames(success, fail) {
-  await api.get("name/random_name").then(success).catch(fail);
-}
-
-export { fetchNames };
---
+++ FE/src/apis/user.js
@@ -0,0 +1,9 @@
+import { ApiInstance } from "./api";
+
+const api = ApiInstance();
+
+function login(success, fail) {
+  // api.get("twitter").then(success).catch(fail);
+}
+
+// export { fetchTweet };
