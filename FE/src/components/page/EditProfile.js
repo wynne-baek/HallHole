400a499887cf1e5e6d74b44ba9cf0a86aeb77c43
SANGBIN IM <97656304+Binzify@users.noreply.github.com>
2022-08-05 11:52:05 +0900

    [Add] 프로필 수정 페이지 제작
    기존 molecule 에서 생성한 캐릭터 프로필과 폼 페이지를 합쳐서 페이지로 제작했습니다

--- FE/src/components/page/EditProfile.js
+++ FE/src/components/page/EditProfile.js
@@ -3,13 +3,24 @@ import { Box } from "@mui/system";
 import CharacterProfile from "../molecule/CharacterProfile";
 import ProfileEdit from "../molecule/ProfileEdit"
 import ButtonStyle from "../atom/Button";
+import TextStyle from "../atom/Text";
 
 export default function EditProfile(props) {
   // 저장, 취소, 비밀번호 변경, 탈퇴하기 버튼 구성하기
   return (
     <Box>
       <CharacterProfile></CharacterProfile>
-      <ProfileEdit></ProfileEdit>
+      <form>
+        <ProfileEdit></ProfileEdit>
+      <Box sx={{ m:3 }}>
+        <Box sx={{ display: "flex", justifyContent: 'space-evenly', mb:2 }}>
+          <TextStyle size="small" variant="black" onClick>비밀번호 변경하기</TextStyle>
+          <TextStyle size="small" variant="red" onClick>회원 탈퇴</TextStyle>
+        </Box>
+        <ButtonStyle size="medium" variant="grey">취소</ButtonStyle>
+        <ButtonStyle size="medium" variant="primary">저장</ButtonStyle>
+      </Box>  
+      </form>
     </Box>  
   );
 }
