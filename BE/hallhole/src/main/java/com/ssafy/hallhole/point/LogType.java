fe6f4686d9948e183d2902845f639ad39ced7a6a
김제관 <jekwan@kakao.com>
2022-08-06 20:57:07 +0900

    Revert "Merge branch 'feature/fe/login-and-signup-api' into 'master'"
    This reverts merge request !40

--- BE/hallhole/src/main/java/com/ssafy/hallhole/point/LogType.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.hallhole.point;
-
-public enum LogType {
-    INCREASE, DECREASE
-}
--- FE/src/components/atom/SpeechBubble.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ChatMsg from '@mui-treasury/components/chatMsg/ChatMsg';
-
-export default function SpeechBubble (props) {
-  <ChatMsg message={props.message}></ChatMsg>
-}
