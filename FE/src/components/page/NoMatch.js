e1e06f9e7965ea955e8203a02889c32c9bfab633
cheecrma <cheecrma@gmail.com>
2022-08-08 15:20:44 +0900

    [Update] noMatch 페이지에 임시로 화면이동 편하게 할 수있도록 해놨습니다.

--- FE/src/components/page/NoMatch.js
+++ FE/src/components/page/NoMatch.js
@@ -15,6 +15,24 @@ export default function NoMatch() {
         <button>돌아가기</button>
         <button onClick={logout}>로그 아웃</button>
       </Link>
+      <Link to="/profile">
+        <button>프로필 화면 가기</button>
+      </Link>
+      <Link to="/followlist">
+        <button>팔로워리스트 확인</button>
+      </Link>
+      <Link to="/forgot">
+        <button>비번 찾기 위한 이메일 페이지</button>
+      </Link>
+      <Link to="/transmit">
+        <button>이메일 전송 확인 페이지</button>
+      </Link>
+      <Link to="/editprofile">
+        <button>개인정보 수정 페이지</button>
+      </Link>
+      <Link to="/editholy">
+        <button>캐릭터 꾸미기 페이지</button>
+      </Link>
     </div>
   );
 }
