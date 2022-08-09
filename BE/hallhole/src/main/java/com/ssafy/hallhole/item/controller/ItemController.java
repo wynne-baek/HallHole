73b5dd72c92cf5fc4ec6c59e84fca0f4cafec401
leemyo <leemyo_@naver.com>
2022-08-10 08:36:41 +0900

    [Add] 아이템 관련 controller, service, repository 생성

---
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/item/controller/ItemController.java
@@ -0,0 +1,24 @@
+package com.ssafy.hallhole.item.controller;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+@RequiredArgsConstructor
+@ApiOperation(value = "itemController")
+public class ItemController {
+
+    // 아이템 목록 모두 가져오기.(아이템 타입도 받아와서 그거에 따라 따로 보여주기)
+
+
+    // 현재 해당 아이디가 보유 중인 아이템 목록 가져오기
+
+
+    // 아이템 추가(상점용)
+
+    // 내 현재 아이템 변경
+
+}
---
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/item/service/ItemService.java
@@ -0,0 +1,4 @@
+package com.ssafy.hallhole.item.service;
+
+public class ItemService {
+}
