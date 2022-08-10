547d80936f17ce5d0e8781dbda7a741ef1b599b8
jekwan <jekwan@kakao.com>
2022-08-10 18:26:42 +0900

    [Update] Text 색상 및 weight 조절

--- FE/src/components/atom/Text.js
+++ FE/src/components/atom/Text.js
@@ -2,12 +2,12 @@ import React from "react";
 import { styled } from "@mui/system";
 
 const CustomText = styled("span")(
-  ({ size, variant }) => `
+  ({ size, variant, weight }) => `
   color: ${getColor(variant)};
   font-size: ${getSize(size)};
   margin: 0;
   padding: 0;
-  font-weight: bold;
+  font-weight: ${getWeight(weight)};
   `,
 );
 
@@ -30,17 +30,33 @@ function getColor(variant) {
       return "#e37373";
     case "black":
       return "black";
+    case "gray":
+    case "grey":
+      return "grey";
     case "white":
       return "white";
+    default:
+      return "#e37373";
   }
 }
 
-export default function TextStyle({ variant, children, size }) {
+function getWeight(weight) {
+  switch (weight) {
+    case "bold":
+      return "bold";
+    case "normal":
+      return "normal";
+    default:
+      return "bold";
+  }
+}
+
+export default function TextStyle({ variant, children, size, weight }) {
   return (
     //  텍스트는 크게 large, medium, small 사이즈로 구분되며
     // 색상은 primary, black, white 로 구분됩니다.
     // 두가지 모두 적용시켜주셔야 합니다.
-    <CustomText variant={variant} size={size}>
+    <CustomText variant={variant} size={size} weight={weight}>
       {children}
     </CustomText>
   );
