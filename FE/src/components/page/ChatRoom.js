8f39bd05f4a20a57e01dd43db212824fb57965e7
jekwan <jekwan@kakao.com>
2022-08-10 13:47:54 +0900

    [Update] 채팅 관련 기본 기능 추가

--- FE/src/components/page/ChatRoom.js
+++ FE/src/components/page/ChatRoom.js
@@ -5,20 +5,91 @@ import { Outlet } from "react-router-dom";
 
 import { Box } from "@mui/material";
 
-import { createWebSocket } from "../../helper/websocket";
+import { getWebsocket } from "../../helper/websocket";
+import { fetchChatLog } from "../../apis/chat";
+
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const CHAT_TYPE = {
+  ENTER: "ENTER",
+  TALK: "TALK",
+  OUT: "OUT",
+};
 
 export default function ChatRoom() {
-  let params = useParams();
-  const ws = createWebSocket();
+  const navigate = useNavigate();
+  const user = useSelector(state => state.user.info);
+  const { chatId } = useParams();
+  const ws = getWebsocket();
+
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  function fetchChatLogSuccess(response) {
+    console.log("채팅 로그 가져오기 성공", response);
+  }
+
+  function fetchChatLogFail(response) {
+    console.log("채팅 로그 가져오기 실패", response);
+  }
+
+  function receiveMessage(response) {
+    console.log("새로운 채팅 메시지 수신", response);
+    const recv = JSON.parse(response.body);
+    setMessages([...messages, recv]);
+  }
+
+  function connectSuccess(frame) {
+    console.log("connected: " + frame);
+    ws.subscribe(`/topic/chat/room/${chatId}`, receiveMessage);
+    sendMessage(CHAT_TYPE.ENTER, "");
+  }
+
+  function connectFail(error) {
+    console.log("connect failed: " + error);
+  }
+
+  function sendMessage(type, message = "") {
+    const msg = {
+      type: type,
+      performanceId: chatId,
+      memberNickName: user.name,
+      message: message,
+      idTag: user.idTag,
+    };
+    console.log(msg);
+    ws.send("/app/chat/message", {}, JSON.stringify(msg));
+  }
+
+  function connect() {
+    console.log("connect");
+    ws.connect({}, connectSuccess, connectFail);
+    fetchChatLog(chatId, fetchChatLogSuccess, fetchChatLogFail);
+  }
+
+  function disconnect() {
+    console.log("disconnect");
+    sendMessage(CHAT_TYPE.OUT);
+    ws.disconnect();
+  }
 
   useEffect(() => {
-    console.log(ws);
+    connect();
+    return disconnect;
   }, []);
 
   return (
     <Box>
-      <Link to="/nomatch">뒤로 가기</Link>
-      This is Chat Room with {params.chatId}
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
+      This is Chat Room with {chatId}
+      <button onClick={() => sendMessage(CHAT_TYPE.TALK, "안녕하세요")}>안녕하세요</button>
       <Outlet />
     </Box>
   );
