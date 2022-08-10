e385c6282074624afd8d696ca6a831aaaad45e50
QuiD <wodnd101@naver.com>
2022-08-10 15:11:02 +0900

    [Update] import 수정

--- BE/hallhole/src/main/java/com/ssafy/hallhole/starterpack/StartController.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/starterpack/StartController.java
@@ -3,7 +3,7 @@ package com.ssafy.hallhole.starterpack;
 import com.ssafy.hallhole.performance.service.PerformanceService;
 import com.ssafy.hallhole.twitter.service.TwitterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class StartController {
     private final PerformanceService performanceService;
 
     @GetMapping
-    public void initStart(){
+    public void initStart() {
         twitterService.getBeforeTweet(30);
         performanceService.scheduledOpenAndCloseChat();
     }
