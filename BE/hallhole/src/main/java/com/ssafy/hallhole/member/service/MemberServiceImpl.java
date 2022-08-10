ba0406c99cad0c645eea092f08b74fbd28feea30
leemyo <leemyo_@naver.com>
2022-08-10 11:34:24 +0900

    [Update] test 위한 비밀번호 이메일 주석처리

--- BE/hallhole/src/main/java/com/ssafy/hallhole/member/service/MemberServiceImpl.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/member/service/MemberServiceImpl.java
@@ -45,11 +45,11 @@ public class MemberServiceImpl implements MemberService {
         Member member = new Member(m.getEmail(),m.getName(),m.getPw());
         duplicateMember(member.getEmail());
 
-        if(!Pattern.matches(
-                "^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\\\(\\\\)\\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\\\(\\\\)\\-_=+]).{8,20}$" , m.getPw()))
-            throw new NotFoundException("비밀번호를 다시 입력해주세요");
-        if(!Pattern.matches("\\w+@\\w+\\.\\w+(\\.\\w+)?", m.getEmail()))
-            throw new NotFoundException("이메일을 다시 입력해주세요.");
+//        if(!Pattern.matches(
+//                "^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\\\(\\\\)\\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\\\(\\\\)\\-_=+]).{8,20}$" , m.getPw()))
+//            throw new NotFoundException("비밀번호를 다시 입력해주세요");
+//        if(!Pattern.matches("\\w+@\\w+\\.\\w+(\\.\\w+)?", m.getEmail()))
+//            throw new NotFoundException("이메일을 다시 입력해주세요.");
 
         char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
                 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
