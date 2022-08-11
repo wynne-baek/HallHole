36d92ebcb5683477d310e874a808353816cb05eb
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-11 10:00:32 +0900

    [Update] follow 기능 구현 완료

--- FE/src/components/organism/ProfileDetail.js
+++ FE/src/components/organism/ProfileDetail.js
@@ -5,27 +5,88 @@ import { Box } from "@mui/system";
 import ProfileImage from "../atom/ProfileImage";
 import TextStyle from "../atom/Text";
 import ButtonStyle from "../atom/Button";
+import { useSelector } from "react-redux";
+
 import { checkFollowStatus, followUser, unfollowUser } from "../../apis/follow";
+import { requestUserInfo } from "../../apis/user";
 
-export default function ProfileDetail({ name, profile, idTag, followerCnt, followingCnt, user }) {
+
+
+export default function ProfileDetail({ id }) {
   const [followStatus, setFollowStatus] = useState("");
-
-  function checkFollowStatusSuccess(res) {
-    console.log(res.data);
-    setFollowStatus(res.data);
-  }
-
-  function checkFollowStatusFail(err) {
-    console.log("팔로우 상태 확인 실패", err);
-  }
+  const user = useSelector(state => state.user.info);
+  const [profileUser, setProfileUser] = useState([]);
 
   useEffect(() => {
-    checkFollowStatus(user?.idTag, idTag, checkFollowStatusSuccess, checkFollowStatusFail);
-  }, [idTag]);
+    requestUserInfo(id, getProfileUserSuccess, getProfileUserFail);
+  }, [followStatus]);
+  
+  function getProfileUserSuccess(res) {
+    setProfileUser(res.data);
+    // console.log("프로필 유저 정보 조회 성공", res);
+  }
+  
+  function getProfileUserFail(err) {
+    // console.log("프로필 유저 정보 조회 실패", err);
+  }
+  
+  // 유저 정보 불러왔는지 확인
+  function validateProfileUser(profileUser) {
+    return profileUser !== [];
+  }
 
+  //팔로우 여부 확인
+  useEffect(() => {
+    checkFollowStatus(user?.idTag, id, checkFollowStatusSuccess, checkFollowStatusFail);
+  }, [user, id]);
+
+  function checkFollowStatusSuccess(res) {
+    // console.log(res.data);
+    setFollowStatus(res.data);
+  }
+  
+  function checkFollowStatusFail(err) {
+    // console.log("팔로우 상태 확인 실패", err);
+  }
+  
+  //팔로우
+  function followSuccess(res) {
+    // console.log("팔로우 성공");
+    setFollowStatus(true);
+  }
+  
+  function followFail(err) {
+    // console.log("팔로우 실패");
+  }
+  
+  function follow(e) {
+    e.preventDefault();
+    followUser(user.idTag, id, followSuccess, followFail);
+  }
+  
+  // 언팔로우
+  function unfollowSuccess(res) {
+    // console.log("언팔로우 성공");
+    setFollowStatus(false);
+  }
+  
+  function unfollowFail(err) {
+    // console.log("언팔로우 실패");
+  }
+  
+  function unfollow(e) {
+    e.preventDefault();
+    unfollowUser(user.idTag, id, unfollowSuccess, unfollowFail);
+  }
+  
+  // 프로필 수정 페이지 이동
+  function editProfile(e) {
+    e.preventDefault();
+  }
+  
   // 프로필 수정, 팔로우, 언팔로우 버튼 설정
   let ProfileButton;
-  if (user?.idTag === idTag) {
+  if (user?.idTag === id) {
     ProfileButton = (
       <ButtonStyle size="small" variant="negative" onClick={editProfile}>
         프로필 수정
@@ -45,59 +106,29 @@ export default function ProfileDetail({ name, profile, idTag, followerCnt, follo
     );
   }
 
-  //팔로우
-  function followSuccess(res) {
-    console.log("팔로우 성공");
-    setFollowStatus(true);
-  }
-
-  function followFail(err) {
-    console.log("팔로우 실패");
-  }
-
-  function follow(e) {
-    e.preventDefault();
-    followUser(user.idTag, idTag, followSuccess, followFail);
-  }
-  // 언팔로우
-  function unfollowSuccess(res) {
-    console.log("언팔로우 성공");
-    setFollowStatus(false);
-  }
-
-  function unfollowFail(err) {
-    console.log("언팔로우 실패");
-  }
-
-  function unfollow(e) {
-    e.preventDefault();
-    unfollowUser(user.idTag, idTag, unfollowSuccess, unfollowFail)
-  }
-
-  function editProfile(e) {
-    e.preventDefault();
-    // 프로필 수정 페이지로 이동
-  }
-
   return (
-    // props로 해당 user 정보를 받아와 나타낼 예정
-    // button은 위 주석처리한 코드와 같이 조건에 따라 문구와 기능을 다르게 할 예정!
     <Box>
-      <CardHeader
-        sx={{ padding: 0.5 }}
-        avatar={<ProfileImage type="large" src=""></ProfileImage>}
-        title={ProfileButton}
-      ></CardHeader>
-      <Box sx={{ ml: 3 }}>
-        <TextStyle size="large">{name} </TextStyle>
-        <TextStyle size="medium">{idTag}</TextStyle>
-        <br></br>
-        <TextStyle size="medium">
-          팔로워 : {followingCnt} | 팔로잉 : {followerCnt}
-        </TextStyle>
-        <br></br>
-        <TextStyle size="small">{profile}</TextStyle>
-      </Box>
+      {validateProfileUser ? (
+        <Box>
+          <CardHeader
+            sx={{ padding: 0.5 }}
+            avatar={<ProfileImage type="large" src=""></ProfileImage>}
+            title={ProfileButton}
+          ></CardHeader>
+          <Box sx={{ ml: 3 }}>
+            <TextStyle size="large">{profileUser.name} </TextStyle>
+            <TextStyle size="medium">{profileUser.idTag}</TextStyle>
+            <br></br>
+            <TextStyle size="medium">
+              팔로워 : {profileUser.followingCnt} | 팔로잉 : {profileUser.followerCnt}
+            </TextStyle>
+            <br></br>
+            <TextStyle size="small">{profileUser.profile}</TextStyle>
+          </Box>
+        </Box>
+      ) : (
+        <Box>로딩중</Box>
+      )}
     </Box>
   );
 }
