f3a894d8d0bd3f79b85fdcf8c7759071077b7310
QuiD <wodnd101@naver.com>
2022-08-12 15:14:22 +0900

    [Add] 매핑

--- BE/hallhole/src/main/java/com/ssafy/hallhole/config/WebSecurityConfig.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/config/WebSecurityConfig.java
@@ -39,6 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception
     {
         web.ignoring().antMatchers("/acc/**", "/char/**");
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/error", "/dist/**");
     }
 
     @Bean
