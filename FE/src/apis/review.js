1968a0586edbc2af103617571c9e4ae4f0462cd8
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-13 15:54:49 +0900

    [Add] review api 작성

---
+++ FE/src/apis/review.js
@@ -0,0 +1,56 @@
+import api from "./api";
+
+function getReviewInfo(reviewId, success, fail) {
+  api.get(`review/${reviewId}`).then(success).catch(fail);
+}
+
+function editReview(contents, performanceId, performance_time, star, title, writerTag, success, fail) {
+  api
+    .post("review/write", {
+      contents: contents,
+      performanceId: performanceId,
+      performance_time: performance_time,
+      star: star,
+      title: title,
+      writerTag: writerTag,
+    })
+    .then(success)
+    .catch(fail);
+}
+
+function deleteReview(writerTag, reviewId, success, fail) {
+  api
+    .put(`review/${reviewId}`, { data: { writerTag: writerTag } })
+    .then(success)
+    .catch(fail);
+}
+
+function getPerformanceReviewList(performanceId, size, start, success, fail) {
+  api
+    .post("review/performance-review-list", { performanceId: performanceId, size: size, start: start })
+    .then(success)
+    .catch(fail);
+}
+
+function getUserReviewList(size, start, writerTag, success, fail) {
+  api
+    .post("review/performance-review-list", { size: size, start: start, writerTag: writerTag })
+    .then(success)
+    .catch(fail);
+}
+
+function writeReview(contents, performanceId, performance_time, star, title, writerTag, success, fail) {
+  api
+    .post("review/write", {
+      contents: contents,
+      performanceId: performanceId,
+      performance_time: performance_time,
+      star: star,
+      title: title,
+      writerTag: writerTag,
+    })
+    .then(success)
+    .catch(fail);
+}
+
+export { getReviewInfo, editReview, deleteReview, getPerformanceReviewList, getUserReviewList, writeReview };
