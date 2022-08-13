2a5a391eaf72df56d716c4cb53d077f434e8740d
leemyo <leemyo_@naver.com>
2022-08-13 18:02:07 +0900

    [Add] security 코드 복구 1차

--- BE/hallhole/src/main/java/com/ssafy/hallhole/config/WebSecurityConfig.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/config/WebSecurityConfig.java
@@ -1,23 +1,41 @@
 package com.ssafy.hallhole.config;
 
-import com.ssafy.hallhole.filter.JwtAuthenticationFilter;
-import com.ssafy.hallhole.member.service.JwtTokenProviderImpl;
+import com.ssafy.hallhole.member.jwt.JwtAccessDeniedHandler;
+import com.ssafy.hallhole.member.jwt.JwtAuthenticationEntryPoint;
+import com.ssafy.hallhole.member.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final JwtTokenProviderImpl jwtTokenService;
+    private final TokenProvider jwtTokenService;
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    private static final String[] PERMIT_URL_ARRAY = {
+            /* swagger v2 */
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            /* swagger v3 */
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
 
 
     @Override
@@ -26,12 +44,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // 일반적인 루트가 아닌 다른 방식으로 요청시 거절, header에 id, pw가 아닌 token(jwt)을 달고 간다.
         // 그래서 basic이 아닌 bearer를 사용한다.
-        http.httpBasic().disable().authorizeRequests() // 요청에 대한 사용권한 체크
-                .antMatchers("/**").permitAll()
+        http.httpBasic().disable() // 요청에 대한 사용권한 체크
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // 시큐리티는 기본적으로 세션을 사용
+                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
                 .and()
-                // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenService),
-                        UsernamePasswordAuthenticationFilter.class);
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
+                .and()
+                .authorizeRequests()
+                .antMatchers("/member/join","/member/login","/member/reissue").permitAll()
+                .antMatchers(PERMIT_URL_ARRAY).permitAll()
+                .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
+
+                // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
 
     }
 
---
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/member/service/UserService.java
@@ -0,0 +1,43 @@
+package com.ssafy.hallhole.member.service;
+
+import com.ssafy.hallhole.advice.exceptions.NotFoundException;
+import com.ssafy.hallhole.member.domain.Member;
+import com.ssafy.hallhole.member.dto.MemberResponseDTO;
+import com.ssafy.hallhole.member.repository.MemberRepository;
+import com.ssafy.hallhole.member.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public MemberResponseDTO getMemberInfo(String email) throws NotFoundException {
+
+        Member member = memberRepository.findByEmail(email);
+        if(member==null || member.isOut()){
+            throw new NotFoundException("유효한 회원이 아닙니다.");
+        }
+
+        MemberResponseDTO responseDto = new MemberResponseDTO(email);
+        return responseDto;
+    }
+
+    // 현재 SecurityContext 에 있는 유저 정보 가져오기
+    @Transactional(readOnly = true)
+    public MemberResponseDTO getMyInfo() throws NotFoundException {
+
+        Member member = memberRepository.findByIdTag(SecurityUtil.getCurrentMemberId());
+        if(member==null || member.isOut()){
+            throw new NotFoundException("로그인 유저 정보가 없습니다.");
+        }
+
+        MemberResponseDTO responseDto = new MemberResponseDTO(member.getEmail());
+        return responseDto;
+    }
+
+}
