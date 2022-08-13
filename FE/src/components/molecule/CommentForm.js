27bbece8c2b5cac53ec85677d1cee14c0eec7446
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-14 00:21:14 +0900

    [Add] CommentForm 컴포넌트 추가 및 api 연결 완료

---
+++ FE/src/components/molecule/CommentForm.js
@@ -0,0 +1,39 @@
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { writeComment } from "../../apis/comment";
+
+import ButtonStyle from "../atom/Button";
+import Input from "../atom/Input";
+
+export default function CommentForm({ reviewId }) {
+  const user = useSelector(state => state.user.info);
+  const [commentContent, setCommentContent] = useState([]);
+
+
+  function writeCommentSuccess(res) {
+    setCommentContent("");
+  }
+
+  function writeCommentFail(err) {}
+  function commentSubmit(e) {
+    e.preventDefault();
+    writeComment(commentContent, user?.idTag, reviewId, writeCommentSuccess, writeCommentFail);
+  }
+
+  return (
+    <Box sx={{ display: "flex", width: "90%", margin: "auto", alignItems: "center" }}>
+      <Input
+        variant="outlined"
+        value={commentContent}
+        size="large"
+        label="Comment"
+        type="text"
+        onChange={e => setCommentContent(e.target.value)}
+      ></Input>
+      <ButtonStyle size="none" onClick={commentSubmit}>
+        작성
+      </ButtonStyle>
+    </Box>
+  );
+}
