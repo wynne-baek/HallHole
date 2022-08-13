ad0e05f5ebd8d0c2eb142ace08cc6ce04dabd9bb
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-13 23:06:33 +0900

    [Update] button 컴포넌트 자체 마진 추가

--- FE/src/components/atom/Button.js
+++ FE/src/components/atom/Button.js
@@ -12,6 +12,7 @@ const style = {
 
 const ButtonCheck = styled(Button)(
   ({ size, variant, color }) => `
+  margin: 3px;
   width: ${getSizeWidth(size)};
   height: ${getSizeHeight(size)};
   background-color: ${getBackgroundColor(variant)};
