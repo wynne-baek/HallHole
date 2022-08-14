8fd647a9318f02b16f11ac39ab2142a661cf7711
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-14 20:39:08 +0900

    [Update] 후기 유무에 따라 조건부 렌더링

--- FE/src/components/organism/ReviewList.js
+++ FE/src/components/organism/ReviewList.js
@@ -7,10 +7,14 @@ import TextStyle from "../atom/Text";
 import ButtonStyle from "../atom/Button";
 
 export default function ReviewList({ performanceReviewList }) {
+  function checkReviewList() {
+    return performanceReviewList === [];
+  }
+
   return (
     <Box>
       <CategoryDivider type="negative" />
-      <Box sx={{my:2}}>
+      <Box sx={{ my: 2 }}>
         <ButtonStyle size="large" variant="primary">
           후기 작성
         </ButtonStyle>
@@ -18,17 +22,27 @@ export default function ReviewList({ performanceReviewList }) {
       <TextStyle size="medium" variant="primary">
         관람 후기
       </TextStyle>
-      {performanceReviewList.map((item, i) => (
-        <ReviewItem
-          key={i}
-          title={item.title}
-          written_date={item.writing_time}
-          star_eval={item.star_eval}
-          user={item.name}
-          writerTag={item.writerTag}
-          id={item.id}
-        ></ReviewItem>
-      ))}
+      {checkReviewList() ? (
+        <Box>
+          {performanceReviewList.map((item, i) => (
+            <ReviewItem
+              key={i}
+              title={item.title}
+              written_date={item.writing_time}
+              star_eval={item.star_eval}
+              user={item.name}
+              writerTag={item.writerTag}
+              id={item.id}
+            ></ReviewItem>
+          ))}
+        </Box>
+      ) : (
+        <Box sx={{ margin: "auto", width: "75%", mt: 3 }}>
+          <TextStyle variant="grey" size="medium">
+            {"아직 작성된 후기가 없어요 :)"}
+          </TextStyle>
+        </Box>
+      )}
     </Box>
   );
 }
