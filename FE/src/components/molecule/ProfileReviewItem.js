d8832260b5a41c4ba1e3dbb3d0f85c6a79a888bd
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-08-15 19:45:59 +0900

    [Update] 리뷰아이템 컴포넌트 수정

--- FE/src/components/molecule/ProfileReviewItem.js
+++ FE/src/components/molecule/ProfileReviewItem.js
@@ -4,13 +4,19 @@ import TextStyle from '../atom/Text'
 
 
 export default function ProfileReviewItem(props) {
+  function changeStrToDate(str) {
+    if (str) {
+      return str.slice(0, 10);
+    }
+  }
+
   return (
     <Box sx={{ width: 1, marginY: 1 }}>
       <div>
-        <TextStyle size="medium" variant="black" sx={{ fontWeight: "bold", mx: 5 }}>[{props.performance_name}] {props.title}</TextStyle>
+        <TextStyle size="medium" variant="black" sx={{ fontWeight: "bold", mx: 5 }}>{props.title}</TextStyle>
       </div>
       <div>
-        <TextStyle size="small" variant="black">{props.star_eval} | {props.date}</TextStyle>
+        <TextStyle size="small" variant="grey">{props.star_eval} | {changeStrToDate(props.writing_time)}</TextStyle>
       </div>
     </Box>
   )
