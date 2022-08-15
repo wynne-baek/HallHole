cc113265d8de809051a48bf774e1b64b92ad87f7
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-08-15 19:37:55 +0900

    [update] test

--- BE/hallhole/src/main/java/com/ssafy/hallhole/member/jwt/JwtFilter.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/member/jwt/JwtFilter.java
@@ -1,9 +1,11 @@
 package com.ssafy.hallhole.member.jwt;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.http.Header;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,6 +13,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
 
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
@@ -18,15 +23,24 @@ public class JwtFilter extends OncePerRequestFilter {
     private final TokenProvider tokenProvider;
 
     // 실제 필터링 로직은 doFilterInternal 에 들어감
-    // JWT 토큰의 인증 정보를 현재 쓰레드의 SecurityContext 에 저장하는 역할 수행펜타곤 키노 키
+    // JWT 토큰의 인증 정보를 현재 쓰레드의 SecurityContext 에 저장하는 역할 수행
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 
         System.out.println("JwtFilter의 doFilterInternal 시작");
         System.out.println("request.getServletPath() = " + request.getServletPath());
 
+
         // 1. Request Header 에서 토큰을 꺼냄
         String accessToken = resolveToken(request);
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()){
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName+"!!!!!!!!!!!!!!!!");
+            System.out.println(request.getHeader(headerName));
+        }
+
         if (accessToken == null) {
             System.out.println("JwtFilter의 doFilterInternal accessToken is null");
         } else {
@@ -42,6 +56,10 @@ public class JwtFilter extends OncePerRequestFilter {
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private String getHeader(@RequestHeader Map<String, Object> requestHeader){
+        return (String) requestHeader.get("token");
     }
 
     // Request Header 에서 토큰 정보를 꺼내오기
