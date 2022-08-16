1d06c89d6e8a4f3b32ed08b9ac97c6608d4e5c55
jekwan <jekwan@kakao.com>
2022-08-16 20:37:43 +0900

    [Update] 채팅 로그 가져오기 API 수정

--- FE/src/apis/chat.js
+++ FE/src/apis/chat.js
@@ -8,8 +8,12 @@ function fetchChatRoom(id, success, fail) {
   api.get(`chat/room/${id}`).then(success).catch(fail);
 }
 
-function fetchChatLog(id, success, fail) {
-  api.get(`chat/logs/${id}`).then(success).catch(fail);
+function fetchChatLog(id, start, size, success, fail) {
+  console.log("fetchChat", start, size);
+  api
+    .get(`chat/logs/${id}`, { params: { start: start, size: size } })
+    .then(success)
+    .catch(fail);
 }
 
 function fetchJoinedChatRoom(id, success, fail) {
