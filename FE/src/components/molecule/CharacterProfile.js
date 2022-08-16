7a69ed74b65f43f3e3816a72cf5090df818db628
SANGBIN IM <97656304+Binzify@users.noreply.github.com>
2022-08-16 22:50:19 +0900

    [Add] Avatar 프로필에 보임
    홀리가 프로필에 보이도록 설정하였습니다.
    - 현 오류 : 프로필 수정과 캐릭터 꾸미기 분리 후 각각 정보가 저장이 안되는 현상이 발생

--- FE/src/components/molecule/CharacterProfile.js
+++ FE/src/components/molecule/CharacterProfile.js
@@ -1,32 +1,54 @@
-import React, {useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
 import CheckroomIcon from '@mui/icons-material/Checkroom';
 import CircleIcon from '@mui/icons-material/Circle';
 import { Box } from "@mui/system";
 
 import ProfileImage from "../atom/ProfileImage";
-import ButtonStyle from "../atom/Button";
+
+import { requestUserInfo } from "../../apis/user";
+
 
 export default function CharacterProfile () {
+  const user = useSelector(state => state.user.info)
+  const [acc, nowAcc] = React.useState(0);
+  const [char, nowChar] = React.useState(0);
+
+  useEffect(() => {
+    requestUserInfo(user?.idTag, getProfileUserSuccess, getProfileUserFail);
+  }, [user]);
+  
+  function getProfileUserSuccess(res) {
+    nowAcc(user?.nowAcc)
+    nowChar(user?.nowChar)
+    console.log(user)
+  }
+  
+  
+  function getProfileUserFail(err) {
+  }
+  
+  
+  const avatarHoly = '/' + char + '_' + acc + '.png'
+  
   const movePage = useNavigate();
 
   function editHoly() {
     movePage(`/editholy`)
   }
   
+
   return (
     <Box sx={{mt:2}}>
       <Box sx={{ position:"relative", display: "flex", justifyContent: 'center', alignItems:'center' }}>
-        <ProfileImage type="large" src=""></ProfileImage>
+        <ProfileImage type="large" src={avatarHoly}></ProfileImage>
         <Box sx={{ position:"absolute", ml: 13, mt: 15}} >
           <CircleIcon sx={{ fontSize: 60, color: "#e37373" }}/>
         </Box>
         <Box sx={{ position:"absolute", ml: 13, mt: 15 }} >
           <CheckroomIcon sx={{ fontSize: 30, color: "white"}} onClick={editHoly}/>
         </Box>
-      {/* </Box>
-      <Box sx={{ mt:1, display: "flex", justifyContent: 'center', alignItems:'center' }}> */}
-        {/* <ButtonStyle size={"medium"} variant={"primary"} onClick={editHoly}>캐릭터 꾸미기</ButtonStyle> */}
       </Box>
     </Box>
   )
