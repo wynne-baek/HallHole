4cc113181c1d59d914f673295022c2e098ed7348
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-08-18 13:38:18 +0900

    [Update] security 경로 수정

--- BE/hallhole/src/main/java/com/ssafy/hallhole/config/WebSecurityConfig.java
+++ BE/hallhole/src/main/java/com/ssafy/hallhole/config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             "/index",
             "/**/*.woff",
             "/**/*.ttf",
-            "/favicon.ico",
+            "/**/favicon.ico",
             "/performance/images",
             "/main",
             "/review/reviewdetail/**",
@@ -51,9 +51,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web){ // 보안 예외처리(HTML, 정적리소스)
         web.ignoring()
-                .antMatchers("/resources/**","/char/**", "/acc/**", "/favicon.ico", "/index_bundle.js","/**/*.css", "/**/*.png", "/**/*.jpg",
+                .antMatchers("/resources/**","/char/**", "/acc/**", "/index_bundle.js","/**/*.css", "/**/*.png", "/**/*.jpg",
                         "/**/*.gif","/**/*.woff", "/**/*.ttf","/performance/images","/member/join","/review/reviewdetail/**","/editprofile/**",
-                        "/member/login");
+                        "/member/login","/**/favicon.ico");
         web.ignoring().antMatchers(PERMIT_URL_ARRAY);
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
@@ -79,11 +79,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .and()
                 .authorizeRequests()
-//                .antMatchers("/member/**", "/comment/**","/facility/**","/follow/**","/item/**","/performance/**","/plike/**","/report/**",
-//                        "/review/**","/review-reaction/**","/twitter/**").hasRole("USER")
-//                .antMatchers(PERMIT_URL_ARRAY).permitAll()
-                .antMatchers("/**").permitAll()
-//                .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
+                .antMatchers("/member/**", "/comment/**","/facility/**","/follow/**","/item/**","/performance/**","/plike/**","/report/**",
+                        "/review/**","/review-reaction/**","/twitter/**").hasRole("USER")
+                .antMatchers(PERMIT_URL_ARRAY).permitAll()
+//                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                 .and()
